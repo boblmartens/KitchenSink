44b5b29ff772881fc6839177bbd8cc0ce0b808b1
Bob Martens <bob@bobmartens.net>
2013-03-27 14:49:19 -0500

    Initial Commit

---
+++ KitchenSink/DSEViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSEViewController.h
+//  KitchenSink
+//
+//  Created by Bob Martens on 3/27/13.
+//  Copyright (c) 2013 Deck78. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSEViewController : UIViewController
+
+@end
---
+++ KitchenSink/DSEViewController.m
@@ -0,0 +1,29 @@
+//
+//  DSEViewController.m
+//  KitchenSink
+//
+//  Created by Bob Martens on 3/27/13.
+//  Copyright (c) 2013 Deck78. All rights reserved.
+//
+
+#import "DSEViewController.h"
+
+@interface DSEViewController ()
+
+@end
+
+@implementation DSEViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
